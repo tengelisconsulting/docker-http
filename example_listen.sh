31195b34ed61bf13653608a30e0413333185d90a
Liam Tengelis <ltengelis@gmail.com>
2020-06-17 17:33:50 +0700

    improve example handler

--- example_listen.sh
+++ example_listen.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
 
+on_url_match() {
+    input=$1
+    url=$2
+    cb=$3
+    if [[ "$(grep ${url} <<< ${input})" ]]; then
+        ${cb} &
+    fi
+}
+
+
+test_handler() {
+    echo "this is the test handler"
+    sleep 2
+    echo "done my work"
+}
 
 while true; do
     input=$(nc -l -p ${LOCAL_HOOK_PORT})
-    echo "input"
-    echo ${input}
+    on_url_match $input "/test" test_handler
 done
